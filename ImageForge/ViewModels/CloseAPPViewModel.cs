fc7d6055179adc664d9d06b03f44b23916669025
志锋 王 <18131372261@163.com>
2026-09-01 21:23:13 +0800

    关闭程序

---
+++ ImageForge/ViewModels/CloseAPPViewModel.cs
@@ -0,0 +1,66 @@
+﻿using Prism.Commands;
+using Prism.Dialogs;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageForge.ViewModels
+{
+	public class CloseAPPViewModel :BindableBase, IDialogAware
+	{
+
+		private string strTitle;
+
+		public string StrTitle
+		{
+			get { return strTitle; }
+			set { strTitle = value; RaisePropertyChanged(); }
+		}
+
+
+		public DelegateCommand YesCommand { get; set; }
+		public DelegateCommand CancelCommnd {  get; set; }
+
+		public  CloseAPPViewModel() 
+		{
+			YesCommand = new DelegateCommand(CloseWindow);
+			CancelCommnd = new DelegateCommand(CancelWindow);
+		}
+
+		private void CancelWindow()
+		{
+			var result = new DialogResult(ButtonResult.Cancel);
+			RequestClose.Invoke(result);
+		}
+
+		private void CloseWindow()
+		{
+
+			var result = new DialogResult(ButtonResult.Yes);
+			RequestClose.Invoke(result);
+		}
+
+		public DialogCloseListener RequestClose { get; }
+
+		public bool CanCloseDialog()
+		{
+			return true;
+		}
+
+		public void OnDialogClosed()
+		{
+			
+		}
+
+		public void OnDialogOpened(IDialogParameters parameters)
+		{
+			if (parameters != null)
+			{
+				StrTitle = parameters["message"].ToString();
+			}
+		}
+	}
+}
--- ImageForge/ViewModels/TopViewModel.cs
+++ ImageForge/ViewModels/TopViewModel.cs
@@ -2,11 +2,65 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Prism.Mvvm;
 using System.Threading.Tasks;
+using Prism.Navigation.Regions;
+using Prism.Events;
+using Prism.Dialogs;
+using Prism.Commands;
 
 namespace ImageForge.ViewModels
 {
-	public class TopViewModel
+	public class TopViewModel : BindableBase, INavigationAware
 	{
+		//消息机制
+		private IEventAggregator eventAggregator;
+
+		private IDialogService dialogService;
+		
+
+		public DelegateCommand CloseCommnd { get; set; }
+		public TopViewModel(IEventAggregator aggregator,IDialogService  dialog)
+		{
+			this.eventAggregator = aggregator;
+			this.dialogService = dialog;
+			CloseCommnd = new DelegateCommand(CloseWindow);
+
+
+		}
+
+		private void CloseWindow()
+		{
+			 DialogParameters keyValues = new DialogParameters();
+			keyValues.Add("message","是否关闭程序");
+
+			dialogService.ShowDialog("CloseAPPView", keyValues, 
+				callback:(result)=>
+				{
+					if (result.Result == ButtonResult.Yes)
+					{
+						App.Current.Shutdown();
+					}
+				}
+				);
+
+		}
+
+ 
+
+		public bool IsNavigationTarget(NavigationContext navigationContext)
+		{
+			return true;
+		}
+
+		public void OnNavigatedFrom(NavigationContext navigationContext)
+		{
+			 
+		}
+
+		public void OnNavigatedTo(NavigationContext navigationContext)
+		{
+			  
+		}
 	}
 }
